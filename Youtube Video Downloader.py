cec529fda96d99d9e45ecbe53664df9476a1fb29
shiwid <ruan.s.gonzalez@gmail.com>
2023-08-22 10:54:34 -0300

    added mp4 function and changed the GUI

--- Youtube Video Downloader.py
+++ Youtube Video Downloader.py
@@ -1,49 +1,60 @@
-from tkinter import *
+import customtkinter
 from tkinter import messagebox
-
+from pytube import YouTube
 
 def video_select():
-    from pytube import YouTube
-    url = website_entry.get()
+    url = entry_1.get()
     yt = YouTube(url)
+    
     if len(url) == 0:
         messagebox.showinfo(
-         title="Erro!",
-            message="Favor insira uma URL válida")
+            title="Erro!",
+            message="Favor insira uma URL válida"
+        )
     else:
         opcao_escolhida = messagebox.askokcancel(
             title=url,
             message=f"O nome do vídeo é: \n "
-              f"{yt.title} \n "
-              f"Pronto para baixar?")
+                    f"{yt.title} \n "
+                    f"Pronto para baixar?"
+        )
+        if opcao_escolhida:
+            optionmenu_1_callback(yt)
 
+def optionmenu_1_callback(yt):
+    value = optionmenu_1.get()
 
-
-
-    if opcao_escolhida:
-        
+    if value == "MP3":
         stream = yt.streams.filter(only_audio=True).get_by_itag("251")
         print(stream)
 
-        yt = yt.streams.get_audio_only()
-        stream.download(filename=f"{yt.title}.mp3",output_path="Downloads do YouTube")
-     
+        stream.download(filename=f"{yt.title}.mp3", output_path="Downloads do YouTube")
+    elif value == "MP4":
+        stream = yt.streams.get_highest_resolution()
+        print(stream)
+        stream.download(filename=f"{yt.title}.mp4", output_path="Downloads do YouTube")
 
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
 
+app = customtkinter.CTk()
+app.geometry("400x240")
+app.title("Editor's Heaven")
 
+app.resizable(False,False)
 
-window = Tk()
-window.title("Shiwid Downloader")
-window.config(padx=10, pady=100)
+frame_1 = customtkinter.CTkFrame(master=app)
+frame_1.pack(pady=20, padx=60, fill="both", expand=True)    
 
-website_label = Label(text="URL:")
-website_label.grid(row=2, column=0)
+entry_1 = customtkinter.CTkEntry(master=frame_1, placeholder_text="URL")
+entry_1.pack(pady=50, padx=10)
 
-website_entry = Entry(width=35)
-website_entry.grid(row=2, column=1, columnspan=2)
-website_entry.focus()
-add_button = Button(text="Baixar Video MP3", width=36, command=video_select)
-add_button.grid(row=4, column=1, columnspan=2)
+button = customtkinter.CTkButton(master=app, text="Baixar", command=video_select, fg_color=("#DB3E39", "#821D1A"))
+button.place(relx=0.5, rely=0.53, anchor=customtkinter.CENTER)
 
+optionmenu_1 = customtkinter.CTkOptionMenu(frame_1, values=["MP3", "MP4"])
+optionmenu_1.pack(pady=10, padx=10)
+optionmenu_1.set("Formato")
 
-window.mainloop()
+app.mainloop()
+
